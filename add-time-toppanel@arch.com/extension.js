b3827aab30b34438424af31d0881d593669a3ec7
Ricardo  Ruiz <ori@ciencias.unam.mx>
2020-03-31 00:36:23 -0600

    Final commit time

--- add-time-toppanel@arch.com/extension.js
+++ add-time-toppanel@arch.com/extension.js
@@ -7,10 +7,12 @@ const Clutter = imports.gi.Clutter;
 
 let text, label;
 let clock, clock_signal_id;
+let y = new Date();
+
 
 function init() {
     clock = new GnomeDesktop.WallClock();
-    label = new St.Label({ text: new Date().getTime().toString()    , y_align: Clutter.ActorAlign.CENTER, x_align: Clutter.ActorAlign.CENTER, style_class: "year-label" });
+    label = new St.Label({ text: y.getTime().toString()    , y_align: Clutter.ActorAlign.CENTER, x_align: Clutter.ActorAlign.CENTER, style_class: "year-label" });
     aggregateMenu = Main.panel.statusArea["aggregateMenu"];
     powerIndicator = aggregateMenu._power.indicators;
 }
@@ -18,16 +20,14 @@ function init() {
 function enable() {
     update_time();
     clock_signal_id = clock.connect('notify::clock', Lang.bind(this, this.update_time));
-    //Main.panel.statusArea.dateMenu.get_children()[0].insert_child_at_index(label,0);
     powerIndicator.add_child(label);
 }
 
 function disable() {
-    Main.panel._centerBox.remove_child(button);
     clock.disconnect(clock_signal_id);
 }
 
 function update_time() {
     var now = new Date();
-    label.set_text((now.getTime().toString()));
+    label.set_text((now.getTime()).toString());
 }
